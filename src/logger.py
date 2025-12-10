1d3b9acc081a8a5e156dec0c3594f7ef4595d366
JaiSinghThakur <jaisingh7051@gmail.com>
2025-12-10 15:18:52 +0530

    Updated .gitignore and project files

--- src/logger.py
+++ src/logger.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
 log_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(log_path,exist_ok=True)
 
@@ -13,4 +14,5 @@ logging.basicConfig(
     format = "[%(asctime)s %(lineno)d %(name)s - %(levelname)s - %(message)s]",
     level = logging.INFO,
 )
+ 
 
